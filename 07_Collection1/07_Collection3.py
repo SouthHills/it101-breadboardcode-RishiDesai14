9112f65962d4b1415c1c43dff7ab3a112c5ebcdc
RishiDesai14 <rdesai37@southhills.edu>
2024-05-01 12:24:25 -0400

    lab7 part3 Done

--- 07_Collection1/07_Collection3.py
+++ 07_Collection1/07_Collection3.py
@@ -1,31 +1,52 @@
 from gpiozero import RGBLED, Button
 import time
 import random
+from signal import pause
 
 # active_high must be true because it is a common anode RGBLed
 LED = RGBLED(red=17, green=18, blue=27, active_high=True)
 BUTTON = Button(23)
-is_blinking = True
+game_started = False
 
 def set_color(r, g, b):
     """ Invert the colors due to using a common anode """
     LED.color = (1 - r, 1 - g, 1 - b)
 
-def initial_test():
-    set_color(1, 0, 0)
-    print("All red")
-    time.sleep(1)
-    set_color(0, 1, 0)
-    print("All green")
-    time.sleep(1)
-    set_color(0, 0, 1)
-    print("All blue")
-    time.sleep(1)
+def start_game():
+    global game_started
+    while True:
+        if game_started:
+            random_color = random.choice([(1, 0, 0), (0, 1, 0), (0, 0, 1)])
+            set_color(*random_color)
+            time.sleep(1)
 
+def end_game(is_winner):
+    if is_winner:
+        for _ in range(5): # Green
+            set_color(0, 1, 0)
+            time.sleep(0.5)
+            set_color(0, 0, 0)
+            time.sleep(0.5)
+    else:
+        for _ in range(5): # Red
+            set_color(1, 0, 0)
+            time.sleep(0.5)
+            set_color(0, 0, 0)
+            time.sleep(0.5)
+    destroy()
+    exit()
 
-def changeLedState():
-    global is_blinking
-    is_blinking = not is_blinking
+def button_pressed():
+    global game_started
+    print("Button pressed")
+    if not game_started:
+        game_started = True
+    else:
+        game_started = False
+        if LED.color == (1, 0, 1):
+            end_game(True)
+        else:
+            end_game(False)
 
 def destroy():
     LED.close()
@@ -33,8 +54,7 @@ def destroy():
 if __name__ == '__main__':  # Program entrance
     print('Program is starting ... ')
     try:
-        BUTTON.when_activated = changeLedState
-        while True:
-            initial_test()
+        BUTTON.when_pressed = button_pressed
+        start_game()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         destroy()
